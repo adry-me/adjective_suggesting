d8409b35e00525833891c157216b54da9773dcdf
ashin590 <ashin2023@gmail.com>
2020-09-03 21:30:43 +0900

    Modified parser to apply to all text files

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from parse_main import load_file
 from nlp.nlp_main import lines_to_sentences, word2vec
 
 if __name__ == '__main__':
-    contents = load_file('P_P_mod.txt')
+    contents = []
+    contents.extend(load_file('data/P_P_mod.txt'))
+    contents.extend(load_file('data/S_S_mod.txt'))
+    contents.extend(load_file('data/M_P_mod.txt'))
 
     from nltk.corpus import movie_reviews
     mv_sentences = [list(s) for s in movie_reviews.sents()]
 
     sentences = lines_to_sentences(contents)
     print(len(sentences))
-    model = word2vec(sentences)
-    vocabs = model.wv.vocab
+    model = word2vec(mv_sentences)
+    vocabs = list(model.wv.vocab.keys())
 
+    print(len(vocabs))
+    print(vocabs)
     print(model.most_similar('he'))
 
     import random
     for _ in range(10):
-        s1 = sentences[random.randint(0, len(sentences) - 1)]
-        s2 = sentences[random.randint(0, len(sentences) - 1)]
-        word1 = s1[random.randint(0, len(s1) - 1)]
-        word2 = s2[random.randint(0, len(s2) - 1)]
+        word1 = vocabs[random.randint(0, len(vocabs) - 1)]
+        word2 = vocabs[random.randint(0, len(vocabs) - 1)]
         sim  = model.wv.similarity(word1, word2)
         print(word1, word2, sim)
